55ecdb89d273783b747cfaa0090087239f096fa6
Tsarev Vladimir <sau412@gmail.com>
2018-04-05 17:03:34 +0300

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,26 @@
+<?php
+$db_host="localhost";
+$db_login="login";
+$db_password="password";
+$db_base="database";
+
+$pool_name="pool name";
+$pool_message="Welcome to $pool_name pool";
+$pool_min_password_length=8;
+
+$boinc_account="your boinc account here";
+// Password hash is
+// echo -n '<password><login>' | md5sum
+$boinc_passwd_hash="your password hash here";
+
+$message_logout_success="Logout successfull";
+$message_login_error="Incorrect login/password";
+
+$message_register_fail="Register fail";
+$message_register_success="Registration complete. Enter your login and password to login";
+
+$message_guide="Guide: 1) Register, 2) Use this pool as account manager, 3) Set your GRC address, 4) Wait for rewards";
+$message_pool_info="Pool $pool_name working!";
+$message_about="About info";
+
+?>
