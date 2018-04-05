fcae0bb844478789ab22d1f6ee63609cb36b78bc
Tsarev Vladimir <sau412@gmail.com>
2018-04-05 16:59:33 +0300

    Create get_project_config.php

---
+++ get_project_config.php
@@ -0,0 +1,13 @@
+<?php
+require_once("settings.php");
+echo <<<_END
+<?xml version="1.0" encoding="UTF-8" ?>
+<project_config>
+    <name>$pool_name</name>
+    <min_passwd_length>$pool_min_password_length</min_passwd_length>
+    <account_manager/>
+    <uses_username/>
+    <client_account_creation_disabled/>
+</project_config>
+_END;
+?>
