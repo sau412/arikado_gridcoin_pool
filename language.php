f5eb6862d891237144bbe11b2e17247b25b90698
Tsarev Vladimir <sau412@gmail.com>
2018-05-07 11:49:43 +0300

    Release candidate

---
+++ language.php
@@ -0,0 +1,25 @@
+<?php
+// Language file
+
+$message_logout_success="Logout successfull";
+$message_login_error="Incorrect login/password";
+
+$message_register_fail="Register fail";
+$message_register_success="Registration complete. Enter your login and password to login";
+
+$message_pool_info="BOINC pool by Vladimir Tsarev, my nickname is sau412 in telegram, twitter, facebook, gmail, github, vk";
+
+$message_change_settings_ok="Settings changed";
+$message_change_settings_password_fail="Change settings fail. Password incorrect";
+$message_change_settings_validation_fail="Change settings fail. Data is not valid";
+
+$message_project_attached="Project attached";
+$message_project_detached="Project detached";
+$message_project_status_changed="Project status changed";
+
+$message_user_status_changed="User status changed";
+
+$message_billing_ok="Billing ok";
+
+$message_bad_token="Bad token";
+?>
