a2538a00a45341e3ad3d03fffd823b4bf01befb1
Tsarev Vladimir <sau412@gmail.com>
2018-05-10 15:41:44 +0300

    New phrases

--- language.php
+++ language.php
@@ -3,11 +3,14 @@
 
 $message_logout_success="Logout successfull";
 $message_login_error="Incorrect login/password";
+$message_xml_error="XML parsing error";
 
 $message_register_fail="Register fail";
 $message_register_success="Registration complete. Enter your login and password to login";
 
-$message_pool_info="BOINC pool by Vladimir Tsarev, my nickname is sau412 in telegram, twitter, facebook, gmail, github, vk";
+$message_pool_info="<p>Guide for new users: 1) register, 2) use this pool as account manager, 3) attach projects, 4) sync with pool, 5) sync one more time after 10 minutes, 6) crunch workunits and wait for rewards (2-5 days)</p><p>Opensource BOINC pool by Vladimir Tsarev, my nickname is sau412 in telegram, twitter, facebook, gmail, github, vk</p>
+<p>Pool stake & DPoR wallet: <a href='https://www.gridcoinstats.eu/address/SDRJWNWezDp6aNGV8m9YchwKPKZ6kLRFRH'>SDRJWNWezDp6aNGV8m9YchwKPKZ6kLRFRH</a></p>
+<p>Pool RPC send reward wallet: <a href='https://www.gridcoinstats.eu/address/S84oWKjbud1vcyft9dy5EAXpLzoNnfnBWY'>S84oWKjbud1vcyft9dy5EAXpLzoNnfnBWY</a></p>";
 
 $message_change_settings_ok="Settings changed";
 $message_change_settings_password_fail="Change settings fail. Password incorrect";
