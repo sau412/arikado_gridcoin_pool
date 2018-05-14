b4205ce83406f82fd9cda053540814dc68c89131
Tsarev Vladimir <sau412@gmail.com>
2018-05-14 12:40:34 +0300

    Less pool info

--- language.php
+++ language.php
@@ -8,9 +8,8 @@ $message_xml_error="XML parsing error";
 $message_register_fail="Register fail";
 $message_register_success="Registration complete. Enter your login and password to login";
 
-$message_pool_info="<p>Guide for new users: 1) register, 2) use this pool as account manager, 3) attach projects, 4) sync with pool, 5) sync one more time after 10 minutes, 6) crunch workunits and wait for rewards (2-5 days)</p><p>Opensource BOINC pool by Vladimir Tsarev, my nickname is sau412 in telegram, twitter, facebook, gmail, github, vk</p>
-<p>Pool stake & DPoR wallet: <a href='https://www.gridcoinstats.eu/address/SDRJWNWezDp6aNGV8m9YchwKPKZ6kLRFRH'>SDRJWNWezDp6aNGV8m9YchwKPKZ6kLRFRH</a></p>
-<p>Pool RPC send reward wallet: <a href='https://www.gridcoinstats.eu/address/S84oWKjbud1vcyft9dy5EAXpLzoNnfnBWY'>S84oWKjbud1vcyft9dy5EAXpLzoNnfnBWY</a></p>";
+$message_pool_info="<p>Guide for new users: 1) register, 2) use this pool as account manager, 3) attach projects, 4) sync with pool, 5) sync one more time after 10 minutes, 6) crunch workunits and wait for rewards</p>
+<p>Write your own pool info here</p>";
 
 $message_change_settings_ok="Settings changed";
 $message_change_settings_password_fail="Change settings fail. Password incorrect";
