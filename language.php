2e7ac81a51d161067020aecd473737f2732580a2
Tsarev Vladimir <sau412@gmail.com>
2018-05-16 12:48:37 +0300

    Messages for register errors

--- language.php
+++ language.php
@@ -5,8 +5,17 @@ $message_logout_success="Logout successfull";
 $message_login_error="Incorrect login/password";
 $message_xml_error="XML parsing error";
 
-$message_register_fail="Register fail";
-$message_register_success="Registration complete. Enter your login and password to login";
+$message_login_fail="Incorrect username or password";
+
+$message_register_fail_login="Register fail. Username validation failed.";
+$message_register_fail_password="Register fail. Password validation failed.";
+$message_register_fail_password_mismatch="Register fail. Password mismatch.";
+$message_register_fail_email="Register fail. E-mail format validation failed.";
+$message_register_fail_grc_address="Register fail. GRC address validation failed.";
+$message_register_fail_db="Register fail. DB error, contact admin.";
+$message_register_fail_username_exists="Register fail. Username exists.";
+
+$message_register_success="Registration complete. Enter your login and password to login.";
 
 $message_pool_info="<p>Guide for new users: 1) register, 2) use this pool as account manager, 3) attach projects, 4) sync with pool, 5) sync one more time after 10 minutes, 6) crunch workunits and wait for rewards</p>
 <p>Write your own pool info here</p>";
