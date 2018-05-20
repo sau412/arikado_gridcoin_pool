2ec71f7a9d49d4a47b8374bd16181323d6e8bfe5
Tsarev Vladimir <sau412@gmail.com>
2018-05-20 12:00:22 +0300

    New hash and salt

--- auth.php
+++ auth.php
@@ -2,14 +2,14 @@
 // Contains authorization and authentification functions
 
 // Costants and messages
-define(AUTH_REGISTER_OK,1);
-define(AUTH_REGISTER_FAIL_LOGIN,2);
-define(AUTH_REGISTER_FAIL_PASSWORD,3);
-define(AUTH_REGISTER_FAIL_PASSWORD_MISMATCH,4);
-define(AUTH_REGISTER_FAIL_EMAIL,5);
-define(AUTH_REGISTER_FAIL_GRC_ADDRESS,6);
-define(AUTH_REGISTER_FAIL_DB,7);
-define(AUTH_REGISTER_FAIL_USERNAME_EXISTS,8);
+define("AUTH_REGISTER_OK",1);
+define("AUTH_REGISTER_FAIL_LOGIN",2);
+define("AUTH_REGISTER_FAIL_PASSWORD",3);
+define("AUTH_REGISTER_FAIL_PASSWORD_MISMATCH",4);
+define("AUTH_REGISTER_FAIL_EMAIL",5);
+define("AUTH_REGISTER_FAIL_GRC_ADDRESS",6);
+define("AUTH_REGISTER_FAIL_DB",7);
+define("AUTH_REGISTER_FAIL_USERNAME_EXISTS",8);
 
 $auth_register_result_to_message=array(
         AUTH_REGISTER_OK=>$message_register_success,
@@ -44,9 +44,10 @@ function auth_is_admin($username) {
 function auth_check_hash($username,$passwd_hash) {
         $username=strtolower($username);
         $username_escaped=db_escape($username);
-        $passwd_hash_escaped=db_escape($passwd_hash);
+        //$passwd_hash_escaped=db_escape($passwd_hash);
+        $passwd_hash_salted=auth_hash_salt($username,$passwd_hash);
 
-        $count=db_query_to_variable("SELECT count(*) FROM `boincmgr_users` WHERE `username`='$username_escaped' AND `passwd_hash`='$passwd_hash_escaped' AND `status`<>'banned'");
+        $count=db_query_to_variable("SELECT count(*) FROM `boincmgr_users` WHERE `username`='$username_escaped' AND `passwd_hash`='$passwd_hash_salted' AND `status`<>'banned'");
         if($count==1) return TRUE;
         else return FALSE;
 }
@@ -66,13 +67,58 @@ function auth_validate_email($email) {
 // Check password format
 function auth_validate_password($password) {
         global $pool_min_password_length;
-        if(strlen($password)>=$pool_min_password_length) return TRUE;
+        if(strlen($password)>=$pool_min_password_length && strlen($password)<=100) return TRUE;
         else return FALSE;
 }
 
 // Check GRC address format
 function auth_validate_grc_address($grc_address) {
         if(preg_match('/^[A-Za-z0-9]{34,34}$/',$grc_address)) return TRUE;
+        else return FALSE;
+}
+
+// Check string has only ASCII characters
+function auth_validate_ascii($string) {
+        if(strlen($string)>100) return FALSE;
+        for($i=0;$i!=strlen($string);$i++) {
+                if(ord($string[$i])<32 || ord($string[$i])>127) return FALSE;
+        }
+        return TRUE;
+}
+
+// Check string is domain name
+function auth_validate_domain($string) {
+        if(strlen($string)>100) return FALSE;
+        return TRUE;
+}
+
+// Check string is integer
+function auth_validate_integer($number) {
+        if(preg_match('/^[0-9]{1,100}$/',$number)) return TRUE;
+        else return FALSE;
+}
+
+// Check string is float (not for exp format)
+function auth_validate_float($number) {
+        if(preg_match('/^[0-9.eE]{1,100}$/',$number)) return TRUE;
+        else return FALSE;
+}
+
+// Check string is md5 hash
+function auth_validate_hash($hash) {
+        if(preg_match('/^[0-9a-fA-F]{32,32}$/',$hash)) return TRUE;
+        else return FALSE;
+}
+
+// Check string is timestamp
+function auth_validate_timestamp($timestamp) {
+        if(preg_match('/^[0-9]{4,4}-[0-9]{1,2}-[0-9]{1,2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}$/',$timestamp)) return TRUE;
+        else return FALSE;
+}
+
+// Check auth_cookie
+function auth_validate_auth_cookie($auth_cookie) {
+        if(preg_match('/^[0-9a-fA-F]{64,64}$/',$auth_cookie)) return TRUE;
         else return FALSE;
 }
 
@@ -98,7 +144,12 @@ function auth_add_user($username,$email,$password_1,$password_2,$grc_address) {
 
         // Add new user
         auth_log("Register username '$username' mail '$email' grc_address '$grc_address'");
-        $result=db_query("INSERT INTO `boincmgr_users` (`username`,`email`,`passwd_hash`,`grc_address`,`status`) VALUES ('$username_escaped','$email_escaped','$password_hash','$grc_address_escaped','user')");
+        $salt=bin2hex(random_bytes(16));
+        $salt_escaped=db_escape($salt);
+        $password_hash_salted=hash("sha256",$password_hash.$salt);
+
+        $result=db_query("INSERT INTO `boincmgr_users` (`username`,`email`,`salt`,`passwd_hash`,`grc_address`,`status`)
+VALUES ('$username_escaped','$email_escaped','$salt_escaped','$password_hash_salted','$grc_address_escaped','user')");
         if($result) return AUTH_REGISTER_OK;
         else return AUTH_REGISTER_FAIL_DB;
 }
@@ -118,7 +169,9 @@ function auth_change_settings($username,$email,$password_1,$password_2,$grc_addr
         if($password_1!='') {
                 $password_hash=auth_hash($username,$password_1);
                 auth_log("Change password username '$username'");
-                $result=db_query("UPDATE `boincmgr_users` SET `email`='$email_escaped',`passwd_hash`='$password_hash',`grc_address`='$grc_address_escaped' WHERE `username`='$username_escaped'");
+                $salt=bin2hex(random_bytes(16));
+                $salt_escaped=db_escape($salt);
+                $result=db_query("UPDATE `boincmgr_users` SET `salt`='$salt_escaped',`email`='$email_escaped',`passwd_hash`='$password_hash',`grc_address`='$grc_address_escaped' WHERE `username`='$username_escaped'");
         } else {
                 $result=db_query("UPDATE `boincmgr_users` SET `email`='$email_escaped',`grc_address`='$grc_address_escaped' WHERE `username`='$username_escaped'");
         }
@@ -132,8 +185,13 @@ function auth_login($username,$password) {
 
         if(auth_check_hash($username,$passwd_hash)) {
                 auth_log("Login username '$username'");
-                setcookie("username",$username);
-                setcookie("passwd_hash",$passwd_hash);
+                $auth_cookie=bin2hex(random_bytes(32));
+                $username_uid=boincmgr_get_username_uid($username);
+                $username_uid_escaped=db_escape($username_uid);
+                $auth_cookie_escaped=db_escape($auth_cookie);
+
+                db_query("INSERT INTO `boincmgr_user_auth_cookies` (`username_uid`,`cookie_token`,`expire_date`) VALUES ('$username_uid_escaped','$auth_cookie_escaped',DATE_ADD(NOW(),INTERVAL 2 DAY))");
+                setcookie("auth_cookie",$auth_cookie);
                 return TRUE;
         } else {
                 auth_log("Login failed username '$username'");
@@ -142,16 +200,26 @@ function auth_login($username,$password) {
 }
 
 // Password to hash
+// Like BOINC, md5($password.lowecase($username))
 function auth_hash($username,$password) {
         return md5($password.strtolower($username));
+}
+
+// Hash password with username salt for check
+function auth_hash_salt($username,$password_hash) {
+        $username_escaped=db_escape($username);
+        $password_hash_escaped=db_escape($password_hash);
+        $salt=db_query_to_variable("SELECT `salt` FROM `boincmgr_users` WHERE `username`='$username_escaped'");
+        return hash("sha256",$password_hash.$salt);
 }
 
 // Logout
 function auth_logout() {
         global $username;
         auth_log("Logout '$username'");
-        setcookie("username","");
-        setcookie("passwd_hash","");
+        setcookie("auth_cookie","");
+//        setcookie("username","");
+//        setcookie("passwd_hash","");
 }
 
 // Generate new token and save to db
@@ -160,6 +228,7 @@ function auth_get_new_token($username) {
 
         $username_escaped=db_escape($username);
         $token=md5(uniqid().$token_salt);
+
         db_query("UPDATE `boincmgr_users` SET `token`='$token' WHERE `username`='$username_escaped'");
         return $token;
 }
