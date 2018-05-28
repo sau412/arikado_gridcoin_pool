e9771a56cc33d056684f25eef57163b67c6b7a73
Tsarev Vladimir <sau412@gmail.com>
2018-05-28 13:17:22 +0300

    Encoding domain, admin can attach and detach

--- boincmgr.php
+++ boincmgr.php
@@ -3,16 +3,18 @@
 
 // Attach project
 function boincmgr_attach($username,$host_uid,$project_uid) {
+//      global $username;
         $project_uid_escaped=db_escape($project_uid);
         $host_uid_escaped=db_escape($host_uid);
         $username_uid=boincmgr_get_username_uid($username);
         $username_uid_escaped=db_escape($username_uid);
 
         // Check if host_uid belongs to this user
-        $host_uid=db_query_to_variable("SELECT `uid` FROM `boincmgr_hosts` WHERE `uid`='$host_uid_escaped' AND `username_uid`='$username_uid_escaped'");
-        if($host_uid) {
+        if(auth_is_admin($username)==FALSE) $host_uid=db_query_to_variable("SELECT `uid` FROM `boincmgr_hosts` WHERE `uid`='$host_uid_escaped' AND `username_uid`='$username_uid_escaped'");
+        if($host_uid || auth_is_admin($username)) {
                 $project_name=boincmgr_get_project_name($project_uid);
                 $host_name=boincmgr_get_host_name($host_uid);
+
                 auth_log("Attach username '$username' project '$project_name' to host '$host_name'");
                 $host_uid_escaped=db_escape($host_uid);
                 db_query("INSERT INTO `boincmgr_attach_projects` (`project_uid`,`host_uid`) VALUES ('$project_uid_escaped','$host_uid_escaped') ON DUPLICATE KEY UPDATE `detach`=0");
@@ -31,10 +33,11 @@ function boincmgr_detach($username,$attached_uid) {
         $username_uid=boincmgr_get_username_uid($username);
         $username_uid_escaped=db_escape($username_uid);
         $host_uid_escaped=db_escape($host_uid);
+        $project_name=boincmgr_get_project_name($project_uid);
 
         // Check if host_uid belongs to this user
-        $host_uid=db_query_to_variable("SELECT `uid` FROM `boincmgr_hosts` WHERE `uid`='$host_uid_escaped' AND `username_uid`='$username_uid_escaped'");
-        if($host_uid) {
+        if(auth_is_admin($username)==FALSE) $host_uid=db_query_to_variable("SELECT `uid` FROM `boincmgr_hosts` WHERE `uid`='$host_uid_escaped' AND `username_uid`='$username_uid_escaped'");
+        if($host_uid || auth_is_admin($username)) {
                 $host_uid_escaped=db_escape($host_uid);
                 $host_name=boincmgr_get_host_name($host_uid);
 
@@ -57,7 +60,12 @@ function boincmgr_get_project_name($uid) {
 // Get host name by uid
 function boincmgr_get_host_name($uid) {
         $uid_escaped=db_escape($uid);
-        return db_query_to_variable("SELECT `domain_name` FROM `boincmgr_hosts` WHERE `uid` = '$uid_escaped'");
+        $hostname_encoded=db_query_to_variable("SELECT `domain_name` FROM `boincmgr_hosts` WHERE `uid` = '$uid_escaped'");
+
+        $hostname_decoded=boincmgr_domain_decode($hostname_encoded);
+
+        if(auth_validate_ascii($hostname_decoded)) return $hostname_decoded;
+        else return $hostname_encoded;
 }
 
 // Get user name by uid
@@ -68,8 +76,8 @@ function boincmgr_get_user_name($uid) {
 
 // Get user uid by username
 function boincmgr_get_username_uid($username) {
-        $username_escaped=db_escape(strtolower($username));
-        return db_query_to_variable("SELECT `uid` FROM `boincmgr_users` WHERE `username` = '$username_escaped'");
+        $username_escaped=db_escape($username);
+        return db_query_to_variable("SELECT `uid` FROM `boincmgr_users` WHERE LOWER(`username`) = LOWER('$username_escaped')");
 }
 
 // Get project uid by project name
@@ -92,6 +100,33 @@ function boincmgr_get_host_uid($username_uid,$host_cpid) {
         $username_uid_escaped=db_escape($username_uid);
         $host_cpid_escaped=db_escape($host_cpid);
         return db_query_to_variable("SELECT `uid` FROM `boincmgr_hosts` WHERE `username_uid` = '$username_uid_escaped' AND `internal_host_cpid`='$host_cpid_escaped'");
+}
+
+// Encode domain to store in DB
+function boincmgr_domain_encode($domain) {
+        return base64_encode($domain);
+}
+
+// Decode domain form DB
+function boincmgr_domain_decode($domain) {
+        return base64_decode($domain);
+}
+
+// Generate prononciable name by host uid
+// Used when hostname has non-ascii characters
+function boincmgr_generate_fict_name($seed,$number) {
+        srand($seed);
+        // Syllables from famous image "your japanese name"
+        // https://i.pinimg.com/originals/3b/7a/bc/3b7abcaea69138e5caace26ea3fa2abe.jpg
+        $parts_array=array("ka","zu","mi","te","ku","lu","ji","ri","ki","zu","me","ta","rin","to","mo","no","ke","shi","ari","chi","do","ru","mei","na","fu","zi");
+        $result="";
+        $parts_count=count($parts_array);
+        for($i=0;$i!=$number;$i++) {
+                $result.=$parts_array[rand(0,$parts_count-1)];
+        }
+        $result=ucfirst($result);
+
+        return $result;
 }
 
 ?>
