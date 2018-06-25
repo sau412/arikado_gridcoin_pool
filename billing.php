d692e3934ac1ae73c8395602afdb8987f682c11f
Tsarev Vladimir <sau412@gmail.com>
2018-06-25 11:02:12 +0300

    Multiple currency support

--- billing.php
+++ billing.php
@@ -12,7 +12,7 @@ function bill_close_period($start_date,$stop_date,$total_reward,$check_rewards) 
         }
 
         $reward_array=array();
-        $enabled_projects_array=db_query_to_array("SELECT `uid`,`name` FROM `boincmgr_projects` WHERE `status` IN ('enabled','auto enabled') ORDER BY `name` ASC");
+        $enabled_projects_array=db_query_to_array("SELECT `uid`,`name` FROM `boincmgr_projects` WHERE `status` IN ('enabled','auto enabled','stats only') ORDER BY `name` ASC");
 
         $proportions_array=bill_calculate_projects_proportion($start_date,$stop_date);
 
@@ -31,12 +31,21 @@ function bill_close_period($start_date,$stop_date,$total_reward,$check_rewards) 
 
         if($check_rewards) echo "Total results:<br>\n";
         // Write rewards to db
-        foreach($reward_array as $grc_address => $reward) {
-                $grc_address_escaped=db_escape($grc_address);
-                $reward_escaped=db_escape($reward);
+        foreach($reward_array as $payout_address => $grc_reward) {
+                $payout_address_escaped=db_escape($payout_address);
+                $payout_currency=db_query_to_variable("SELECT `currency` FROM `boincmgr_users` WHERE `payout_address`='$payout_address_escaped'");
+                $rate=boincmgr_get_payout_rate($payout_currency);
+                $grc_reward_escaped=db_escape($grc_reward);
+                $amount=$grc_reward*$rate;
+
                 $billing_uid_escaped=db_escape($billing_uid);
-                if($check_rewards) echo "$grc_address $reward<br>\n";
-                else db_query("INSERT INTO `boincmgr_payouts` (`billing_uid`,`grc_address`,`amount`) VALUES ('$billing_uid_escaped','$grc_address_escaped','$reward_escaped')");
+                $payout_currency_escaped=db_escape($payout_currency);
+                $rate_escaped=db_escape($rate);
+                $amount_escaped=db_escape($amount);
+
+                if($check_rewards) echo "Address $payout_address GRC reward $grc_reward currency $payout_currency rate $rate result amount $amount<br>\n";
+                else db_query("INSERT INTO `boincmgr_payouts` (`billing_uid`,`payout_address`,`grc_amount`,`currency`,`rate`,`amount`)
+VALUES ('$billing_uid_escaped','$payout_address_escaped','$grc_reward_escaped','$payout_currency_escaped','$rate_escaped','$amount_escaped')");
         }
         if($check_rewards) {
                 flush();
@@ -50,7 +59,7 @@ function bill_calculate_projects_proportion($start_date,$stop_date) {
         $stop_date_escaped=db_escape($stop_date);
         $pre_result=array();
         $result=array();
-        $enabled_projects_array=db_query_to_array("SELECT `uid`,`name` FROM `boincmgr_projects` WHERE `status` IN ('enabled','auto enabled')");
+        $enabled_projects_array=db_query_to_array("SELECT `uid`,`name` FROM `boincmgr_projects` WHERE `status` IN ('enabled','auto enabled','stats only')");
         $contrib_sum=0;
         foreach($enabled_projects_array as $project) {
                 $project_uid=$project['uid'];
@@ -99,30 +108,24 @@ LEFT JOIN `boincmgr_users` AS bu ON bu.`uid`=bh.`username_uid`
 WHERE bphs.`project_uid`='$project_uid_escaped' AND bu.`status` IN ('user','admin') AND bphs.`timestamp`>'$start_date_escaped' AND bphs.`timestamp`<='$stop_date_escaped'");
         if($check_rewards) echo "Project RAC $project_total_rac<br>\n";
         if($project_total_rac!=0) {
-/*              $user_stats_array=db_query_to_array("SELECT bu.`grc_address`,SUM(`expavg_credit`) AS sum_credit FROM `boincmgr_project_host_stats` AS bphs
-LEFT JOIN `boincmgr_host_projects` AS bhp ON bhp.`host_uid`=bphs.`host_uid` AND bhp.`host_id`=bphs.`host_id` AND bhp.`project_uid`=bphs.`project_uid`
-LEFT JOIN `boincmgr_hosts` AS bh ON bh.`uid`=bhp.`host_uid`
-LEFT JOIN `boincmgr_users` AS bu ON bu.`uid`=bh.`username_uid`
-WHERE bphs.`project_uid`='$project_uid' AND `status` IN ('user','admin') AND bphs.`timestamp`>'$start_date_escaped' AND bphs.`timestamp`<='$stop_date_escaped'
-GROUP BY bu.`grc_address`");*/
-                $user_stats_array=db_query_to_array("SELECT bu.`grc_address`,SUM(bphs.`expavg_credit`) AS sum_credit FROM `boincmgr_project_host_stats` AS bphs
+                $user_stats_array=db_query_to_array("SELECT bu.`payout_address`,SUM(bphs.`expavg_credit`) AS sum_credit FROM `boincmgr_project_host_stats` AS bphs
 LEFT JOIN `boincmgr_hosts` AS bh ON bh.`uid`=bphs.`host_uid`
 LEFT JOIN `boincmgr_users` AS bu ON bu.`uid`=bh.`username_uid`
 WHERE bphs.`project_uid`='$project_uid_escaped' AND bu.`status` IN ('user','admin') AND bphs.`timestamp`>'$start_date_escaped' AND bphs.`timestamp`<='$stop_date_escaped'
-GROUP BY bu.`grc_address`");
+GROUP BY bu.`payout_address`");
 
                 foreach($user_stats_array as $user_data) {
                         $host_rac=$user_data['sum_credit'];
-                        $grc_address=$user_data['grc_address'];
+                        $payout_address=$user_data['payout_address'];
                         if($host_rac==0) continue;
 
                         $user_reward=($project_reward/$project_total_rac)*$host_rac;
                         if($user_reward==0) continue;
 
-                        $grc_address_html=html_escape($grc_address);
-                        if($check_rewards) echo "GRC address '$grc_address' RAC $host_rac reward $user_reward<br>\n";
+                        $payout_address_html=html_escape($payout_address);
+                        if($check_rewards) echo "Payout address '$payout_address' RAC $host_rac reward $user_reward<br>\n";
 
-                        if($grc_address!='') $reward_array[$grc_address]=$user_reward;
+                        if($payout_address!='') $reward_array[$payout_address]=$user_reward;
                 }
         }
         if($check_rewards) {
@@ -135,9 +138,9 @@ GROUP BY bu.`grc_address`");
 // Combine rewards arrays to avoid multiple payouts to single address
 function reward_array_combine($reward_array_1,$reward_array_2) {
         $reward_array=$reward_array_1;
-        foreach($reward_array_2 as $grc_address => $reward) {
-                if(isset($reward_array[$grc_address])) $reward_array[$grc_address]+=$reward;
-                else $reward_array[$grc_address]=$reward;
+        foreach($reward_array_2 as $payout_address => $reward) {
+                if(isset($reward_array[$payout_address])) $reward_array[$payout_address]+=$reward;
+                else $reward_array[$payout_address]=$reward;
         }
         return $reward_array;
 }
