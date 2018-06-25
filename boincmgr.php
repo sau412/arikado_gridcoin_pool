882860d4145982e4484def0c6f0edeb996046e9d
Tsarev Vladimir <sau412@gmail.com>
2018-06-25 11:03:18 +0300

    Multiple currency support, project settings

--- boincmgr.php
+++ boincmgr.php
@@ -20,7 +20,6 @@ function boincmgr_attach($username,$host_uid,$project_uid) {
         }
         if(auth_is_admin($username) || ($host_correct && $project_correct)) {
                 auth_log("Attach username '$username' project '$project_name' to host '$host_name'");
-                $host_uid_escaped=db_escape($host_uid);
                 db_query("INSERT INTO `boincmgr_attach_projects` (`project_uid`,`host_uid`,`status`) VALUES ('$project_uid_escaped','$host_uid_escaped','new') ON DUPLICATE KEY UPDATE `status`='new'");
                 return TRUE;
         } else {
@@ -44,11 +43,9 @@ function boincmgr_detach($username,$attached_uid) {
         // Check if host_uid belongs to this user
         if(auth_is_admin($username)==FALSE) $host_uid=db_query_to_variable("SELECT `uid` FROM `boincmgr_hosts` WHERE `uid`='$host_uid_escaped' AND `username_uid`='$username_uid_escaped'");
         if($host_uid || auth_is_admin($username)) {
-                $host_uid_escaped=db_escape($host_uid);
                 $host_name=boincmgr_get_host_name($host_uid);
 
                 auth_log("Detach username '$username' project '$project_name' from host '$host_name'");
-                $host_uid_escaped=db_escape($host_uid);
 
                 // If status is "new" then we can just delete
                 if($status=="new") {
@@ -57,6 +54,51 @@ function boincmgr_detach($username,$attached_uid) {
                 } else {
                         db_query("UPDATE `boincmgr_attach_projects` SET `status`='detach' WHERE `uid`='$attached_uid_escaped'");
                 }
+                return TRUE;
+        } else {
+                return FALSE;
+        }
+}
+
+// Settings for project
+function boincmgr_set_project_settings($username,$attached_uid,$resource_share,$options_array) {
+        var_dump($username,$attached_uid,$resource_share,$options_array);
+
+        $attached_uid_escaped=db_escape($attached_uid);
+        $host_uid=db_query_to_variable("SELECT `host_uid` FROM `boincmgr_attach_projects` WHERE `uid`='$attached_uid_escaped'");
+        $project_uid=db_query_to_variable("SELECT `project_uid` FROM `boincmgr_attach_projects` WHERE `uid`='$attached_uid_escaped'");
+        $status=db_query_to_variable("SELECT `status` FROM `boincmgr_attach_projects` WHERE `uid`='$attached_uid_escaped'");
+
+        $username_uid=boincmgr_get_username_uid($username);
+        $username_uid_escaped=db_escape($username_uid);
+        $host_uid_escaped=db_escape($host_uid);
+        $project_name=boincmgr_get_project_name($project_uid);
+
+        // Check if host_uid belongs to this user
+        if(auth_is_admin($username)==FALSE) $host_uid=db_query_to_variable("SELECT `uid` FROM `boincmgr_hosts` WHERE `uid`='$host_uid_escaped' AND `username_uid`='$username_uid_escaped'");
+        if($host_uid || auth_is_admin($username)) {
+                $host_name=boincmgr_get_host_name($host_uid);
+
+                $resource_share_escaped=db_escape($resource_share);
+                $options_str_array=array();
+                $valid_options_array=array("detach","detach_when_done","suspend","dont_request_more_work","abort_not_started","no_cpu","no_cuda","no_ati","no_intel");
+                foreach($valid_options_array as $valid_option) {
+                        if(in_array($valid_option,$options_array)) $options_str_array[]=$valid_option;
+                }
+                $options_str=implode(",",$options_str_array);
+                $options_str_escaped=db_escape($options_str);
+
+                auth_log("Change project settings username '$username' project '$project_name' host '$host_name' resource share '$resource_share' options '$options_str'");
+
+                db_query("UPDATE `boincmgr_attach_projects` SET `resource_share`='$resource_share_escaped',`options`='$options_str_escaped' WHERE `uid`='$attached_uid_escaped'");
+
+                // If detach or detach_when_done, then update status
+                if(in_array("detach",$options_array) || in_array("detach_when_done",$options_array)) {
+                        db_query("UPDATE `boincmgr_attach_projects` SET `status`='detach' WHERE `uid`='$attached_uid_escaped'");
+                } else {
+                        db_query("UPDATE `boincmgr_attach_projects` SET `status`='sent' WHERE `uid`='$attached_uid_escaped'");
+                }
+
                 return TRUE;
         } else {
                 return FALSE;
@@ -154,13 +196,25 @@ function boincmgr_delete_host($username,$host_uid) {
 
 // Set pool info
 function boincmgr_set_pool_info($pool_info) {
-        $pool_info_escaped=db_escape($pool_info);
-        db_query("UPDATE `boincmgr_variables` SET `value`='$pool_info_escaped' WHERE `name`='pool_info'");
+        boincmgr_set_variable("pool_info",$pool_info);
 }
 
 // Get pool info
 function boincmgr_get_pool_info() {
-        return db_query_to_variable("SELECT `value` FROM `boincmgr_variables` WHERE `name`='pool_info'");
+        return boincmgr_get_variable("pool_info");
+}
+
+// Get variable
+function boincmgr_get_variable($name) {
+        $name_escaped=db_escape($name);
+        return db_query_to_variable("SELECT `value` FROM `boincmgr_variables` WHERE `name`='$name_escaped'");
+}
+
+// Set variable
+function boincmgr_set_variable($name,$value) {
+        $name_escaped=db_escape($name);
+        $value_escaped=db_escape($value);
+        return db_query("INSERT INTO `boincmgr_variables` (`name`,`value`) VALUES ('$name_escaped','$value_escaped') ON DUPLICATE KEY UPDATE `value`=VALUES(`value`)");
 }
 
 // Clear project log
@@ -198,6 +252,66 @@ function boincmgr_cache_function($function_name,$parameters) {
 ON DUPLICATE KEY UPDATE `content`=VALUES(`content`),`valid_until`=DATE_ADD(NOW(),INTERVAL 1 HOUR)");
         }
         return $result;
+}
+
+// Get payout rate
+function boincmgr_get_payout_rate($currency) {
+        switch($currency) {
+                case "GRC":
+                        return 1;
+                case "DOGE":
+                        $btc_grc_rate=boincmgr_get_variable("BTC_GRC");
+                        $btc_doge_rate=boincmgr_get_variable("BTC_DOGE");
+                        if($btc_doge_rate!=0) $doge_grc_rate=$btc_grc_rate/$btc_doge_rate;
+                        else $doge_grc_rate=0;
+                        return $doge_grc_rate;
+                case "LTC":
+                        $btc_grc_rate=boincmgr_get_variable("BTC_GRC");
+                        $btc_ltc_rate=boincmgr_get_variable("BTC_LTC");
+                        if($btc_ltc_rate!=0) $ltc_grc_rate=$btc_grc_rate/$btc_ltc_rate;
+                        else $ltc_grc_rate=0;
+                        return $ltc_grc_rate;
+                case "ETH":
+                        $btc_grc_rate=boincmgr_get_variable("BTC_GRC");
+                        $btc_eth_rate=boincmgr_get_variable("BTC_ETH");
+                        if($btc_eth_rate!=0) $eth_grc_rate=$btc_grc_rate/$btc_eth_rate;
+                        else $eth_grc_rate=0;
+                        return $eth_grc_rate;
+                case "WMZ":
+                        $btc_grc_rate=boincmgr_get_variable("BTC_GRC");
+                        $usdt_btc_rate=boincmgr_get_variable("USDT_BTC");
+                        $wmz_grc_rate=$btc_grc_rate*$usdt_btc_rate;
+                        return $wmz_grc_rate;
+                case "XMR":
+                        $btc_grc_rate=boincmgr_get_variable("BTC_GRC");
+                        $btc_xmr_rate=boincmgr_get_variable("BTC_XMR");
+                        if($btc_xmr_rate!=0) $xmr_grc_rate=$btc_grc_rate/$btc_xmr_rate;
+                        else $xmr_grc_rate=0;
+                        return $xmr_grc_rate;
+                case "BTC":
+                        $btc_grc_rate=boincmgr_get_variable("BTC_GRC");
+                        return $btc_grc_rate;
+                default:
+                        return 0;
+        }
+}
+
+// Get payout limit
+function boincmgr_get_payout_limit($currency) {
+        $currency_escaped=db_escape($currency);
+        return db_query_to_variable("SELECT `payout_limit` FROM `boincmgr_currency` WHERE `name`='$currency_escaped'");
+}
+
+// Get payout limit
+function boincmgr_get_tx_fee_estimation($currency) {
+        $currency_escaped=db_escape($currency);
+        return db_query_to_variable("SELECT `tx_fee` FROM `boincmgr_currency` WHERE `name`='$currency_escaped'");
+}
+
+// Get payout fee
+function boincmgr_get_service_fee($currency) {
+        $currency_escaped=db_escape($currency);
+        return db_query_to_variable("SELECT `project_fee` FROM `boincmgr_currency` WHERE `name`='$currency_escaped'");
 }
 
 ?>
