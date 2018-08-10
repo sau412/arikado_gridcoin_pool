6e66c04eb9afc3047ea9bf975683a7d4c1cdd7ef
Tsarev Vladimir <sau412@gmail.com>
2018-08-10 16:46:12 +0300

    New pull-down menu item

--- common.js
+++ common.js
@@ -55,7 +55,7 @@ function toggle_block(id) {
 
 // Hide all submenu (for menu)
 function hide_all_submenu(id) {
-        var blocks_array = ["boinc","control","info"];
+        var blocks_array = ["boinc","control","info","ratings"];
         blocks_array.forEach(function(element) {
                 if(document.getElementById(element) != null && element != id) document.getElementById(element).style.display='none';
         });
