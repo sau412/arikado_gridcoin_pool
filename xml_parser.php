b59bdcdf7f0faf92a8029ff4b6129f8e580116c6
Tsarev Vladimir <sau412@gmail.com>
2018-08-10 16:30:49 +0300

    Parsing new host info tags

--- xml_parser.php
+++ xml_parser.php
@@ -10,12 +10,15 @@ function xml_parse_user_request($xml) {
         $strings_array=explode("\n",$xml);
         $global_tags_array=array("name","password_hash","host_cpid");
         $project_tags_array=array("project_name","account_key","hostid");
-        $host_tags_array=array("domain_name","host_cpid","p_model","p_ncpus","n_usable_coprocs");
+        $host_tags_array=array("domain_name","host_cpid","p_model","p_ncpus","n_usable_coprocs","os_name","os_version","virtualbox_version","product_name");
+        $gpu_tags_array=array("count","name");
         $project_index=0;
+        $gpu_index=0;
         $tag_flag=FALSE;
         $section="global";
         foreach($strings_array as $str) {
                 $str=trim($str);
+                // Projects section
                 if(preg_match("/^<project>$/",$str)) {
                         $section="projects";
                         $project_index++;
@@ -23,8 +26,35 @@ function xml_parse_user_request($xml) {
                 else if(preg_match("/^<working_global_preferences>$/",$str)) {
                         $section="working_global_preferences";
                 }
+                // Host info section
                 else if(preg_match("/^<host_info>$/",$str)) {
                         $section="host_info";
+                }
+                // GPU section
+                else if(preg_match("/^<coproc_intel_gpu>$/",$str)) {
+                        $section="gpu_info";
+                        $gpu_index++;
+                }
+                else if(preg_match("/^<coproc_cuda>$/",$str)) {
+                        $section="gpu_info";
+                        $gpu_index++;
+                }
+                else if(preg_match("/^<coproc_ati>$/",$str)) {
+                        $section="gpu_info";
+                        $gpu_index++;
+                }
+                // End of GPU section
+                else if(preg_match("/^<\\/coproc_intel_gpu>$/",$str)) {
+                        $section="global";
+                        $gpu_index++;
+                }
+                else if(preg_match("/^<\\/coproc_cuda>$/",$str)) {
+                        $section="global";
+                        $gpu_index++;
+                }
+                else if(preg_match("/^<\\/coproc_ati>$/",$str)) {
+                        $section="global";
+                        $gpu_index++;
                 }
                 if($section=="global") {
                         if($tag_flag==FALSE) {
@@ -59,6 +89,18 @@ function xml_parse_user_request($xml) {
                                 }
                         } else {
                                 $result[$tag_flag]=$str;
+                                $tag_flag=FALSE;
+                        }
+                }
+                else if($section=="gpu_info") {
+                        if($tag_flag==FALSE) {
+                                foreach($gpu_tags_array as $tag) {
+                                        if(preg_match("/^<($tag)>\$/",$str,$matches)) {
+                                                $tag_flag=$matches[1];
+                                        }
+                                }
+                        } else {
+                                $result["gpus"][$gpu_index][$tag_flag]=$str;
                                 $tag_flag=FALSE;
                         }
                 }
