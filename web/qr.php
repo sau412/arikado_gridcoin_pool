4a75b2fd89d0a44a78d3b9ae262bd4afd2af4951
Tsarev Vladimir <sau412@gmail.com>
2019-03-28 21:54:02 +0300

    Regexp fix

--- web/qr.php
+++ web/qr.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-type: image/png");
 $str=stripslashes($_GET['str']);
-if(preg_match('/[0-9A-Za-z]{1,20}/',$str)) {
+if(preg_match('/^[0-9A-Za-z]{1,20}$/',$str)) {
         passthru("qrencode '$str' -o -");
 }
 ?>
