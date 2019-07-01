5a8fb50e3d34e3ccd38c785ed70222180383e8ac
Tsarev Vladimir <sau412@gmail.com>
2019-07-01 13:01:03 +0300

    Table name change

--- tasks/update_graphs.php
+++ tasks/update_graphs.php
@@ -2,22 +2,22 @@
 // Update graphs cache
 if(!isset($argc)) die();
 
-require_once("settings.php");
-require_once("db.php");
-require_once("boincmgr.php");
-require_once("auth.php");
-require_once("canvas.php");
+require_once("../lib/settings.php");
+require_once("../lib/db.php");
+require_once("../lib/boincmgr.php");
+require_once("../lib/auth.php");
+require_once("../lib/canvas.php");
 
 db_connect();
 
 echo "Updating host graphs\n";
-$host_uid_array=db_query_to_array("SELECT `uid` FROM `boincmgr_hosts`");
+$host_uid_array=db_query_to_array("SELECT `uid` FROM `hosts`");
 $index=1;
 foreach($host_uid_array as $host_uid_row) {
         echo "Host $index of ".count($host_uid_array)."\n";
         $host_uid=$host_uid_row['uid'];
         boincmgr_cache_function("canvas_graph_host_all_projects",array($host_uid),TRUE);
-        $project_uid_array=db_query_to_array("SELECT `uid` FROM `boincmgr_projects`");
+        $project_uid_array=db_query_to_array("SELECT `uid` FROM `projects`");
         foreach($project_uid_array as $project_uid_row) {
                 $project_uid=$project_uid_row['uid'];
                 boincmgr_cache_function("canvas_graph_host_project",array($host_uid,$project_uid),TRUE);
@@ -27,13 +27,13 @@ foreach($host_uid_array as $host_uid_row) {
 echo "\n";
 
 echo "Updating users graphs\n";
-$user_uid_array=db_query_to_array("SELECT `uid` FROM `boincmgr_users`");
+$user_uid_array=db_query_to_array("SELECT `uid` FROM `users`");
 $index=0;
 foreach($user_uid_array as $user_uid_row) {
         echo "User $index of ".count($user_uid_array)."\n";
         $user_uid=$user_uid_row['uid'];
         boincmgr_cache_function("canvas_graph_username",array($user_uid),TRUE);
-        $project_uid_array=db_query_to_array("SELECT `uid` FROM `boincmgr_projects`");
+        $project_uid_array=db_query_to_array("SELECT `uid` FROM `projects`");
         foreach($project_uid_array as $project_uid_row) {
                 $project_uid=$project_uid_row['uid'];
                 boincmgr_cache_function("canvas_graph_username_project",array($user_uid,$project_uid),TRUE);
