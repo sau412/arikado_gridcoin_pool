850059ea52cdcc4fb988f114ef0c6e111f5b9902
sau412 <sau412@protonmail.com>
2020-04-17 12:19:30 +0300

    Semicolon fix

--- lib/boincmgr.php
+++ lib/boincmgr.php
@@ -470,7 +470,7 @@ function boincmgr_message_send($username_uid,$reply_to,$message) {
 		$email .= "Message: $message\n";
 		
 		$email_escaped = db_escape($email);
-		$feedback_email_escaped = db_escape($feedback_email)
+		$feedback_email_escaped = db_escape($feedback_email);
 		
 		db_query("INSERT INTO `email` (`to`,`subject`,`message`) VALUES ('$feedback_email','Pool feedback query','$message_escaped')");
 	}
