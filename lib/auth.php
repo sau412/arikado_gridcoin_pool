3ce120ef66d98358b8a1dab2f39a524e8658bd9f
sau412 <sau412@protonmail.com>
2020-06-25 23:08:36 +0300

    Logging direct to logger

--- lib/auth.php
+++ lib/auth.php
@@ -318,13 +318,23 @@ function auth_check_token($username,$token) {
 
 // Write action to log
 function auth_log($message, $severity = 7) {
-	global $project_log_name;
-	
-	broker_add("logger", [
-		"source" => $project_log_name,
-		"severity" => $severity,
-		"message" => $message,
-	]);
+        global $logger_url;
+        global $project_log_name;
+
+		if(is_array($message)) $message = json_encode($message);
+
+        $ch = curl_init($logger_url);
+        $body = json_encode([
+        	"source" => $project_log_name,
+        	"severity" => $severity,
+        	"message" => $message]);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        $result_json = curl_exec($ch);
+        curl_close($ch);        
+        $result = json_decode($result_json, true);
+        return $result;
 }
 
 // Write debug log
