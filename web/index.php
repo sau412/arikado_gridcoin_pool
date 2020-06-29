0e1ad16807c244b972515487bd93c19559442afb
sau412 <sau412@protonmail.com>
2020-06-29 17:28:20 +0300

    Email lib included

--- web/index.php
+++ web/index.php
@@ -9,6 +9,7 @@ require_once("../lib/canvas.php");
 require_once("../lib/xml_parser.php");
 require_once("../lib/captcha.php");
 require_once("../lib/broker.php");
+require_once("../lib/email.php");
 
 db_connect();
 
