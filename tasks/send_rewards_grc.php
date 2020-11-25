5d81ba5cbe4a5ed6e9577de9a5bc5725f54ebad8
sau412 <sau412@protonmail.com>
2020-11-25 09:46:16 +0300

    Payout address shouldn't be empty for payout

--- tasks/send_rewards_grc.php
+++ tasks/send_rewards_grc.php
@@ -50,7 +50,7 @@ $payout_data_array=db_query_to_array("SELECT
 	SUM(`amount`) AS amount_sum,
 	`wallet_send_uid`
 FROM `payouts`
-WHERE `currency` IN ('GRC','GRC2') AND (`txid` IS NULL OR `txid`='')
+WHERE `currency` IN ('GRC','GRC2') AND (`txid` IS NULL OR `txid`='') AND (`payout_address` != '')
 GROUP BY `user_uid`,`payout_address`,`currency`,`wallet_send_uid`
 HAVING SUM(`amount`)>='$minimal_amount_escaped'");
 
