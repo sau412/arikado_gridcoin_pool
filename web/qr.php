9c0249a1c919f7b157992689e0abe5b2f4b86082
v.tsarev <v.tsarev@auvix.ru>
2022-12-26 18:49:30 +0700

    QR code generator improvement 2

--- web/qr.php
+++ web/qr.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-type: image/png");
 $str=stripslashes($_GET['str']);
-if(preg_match('/^[0-9A-Za-z_:\\/ \\?]{1,150}$/',$str)) {
+if(preg_match('/^[0-9A-Za-z_:\\/ \\?\\.]{1,150}$/',$str)) {
         passthru("qrencode '$str' -o -");
 }
