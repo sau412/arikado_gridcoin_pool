13f2a5e5d9ffc8782a9a148ca96b32ed9afec396
sau412 <sau@arikado.xyz>
2023-05-12 14:20:00 +0300

    Variable name fix

--- lib/auth.php
+++ lib/auth.php
@@ -354,7 +354,7 @@ function auth_log($messageRaw, $severity = 7) {
 	global $project_log_name;
 
 	openlog('php', LOG_ODELAY, LOG_USER);
-	if(gettype($message) !== "string") {
+	if(gettype($messageRaw) !== "string") {
 			$message = json_encode($messageRaw);
 	}
 	else {
